36ef4cdbb188a81e4433cb71476b2e9eb09a2c36
shx-dow <83426772+shx-dow@users.noreply.github.com>
2026-06-03 19:56:10 +0530

    Add base58 short URL generator

--- shortener/shorturl_generator.go
+++ shortener/shorturl_generator.go
@@ -2,6 +2,11 @@ package shortener
 
 import (
 	"crypto/sha256"
+	"fmt"
+	"math/big"
+	"os"
+
+	"github.com/itchyny/base58-go"
 )
 
 func sha256Of(input string) []byte {
@@ -10,3 +15,19 @@ func sha256Of(input string) []byte {
 	return algorithm.Sum(nil)
 }
 
+func base58Encoded(bytes []byte) string {
+	encoding := base58.BitcoinEncoding
+	encoded, err := encoding.Encode(bytes)
+	if err != nil {
+		fmt.Println(err.Error())
+		os.Exit(1)
+	}
+	return string(encoded)
+}
+
+func GenerateShortLink(intialLink string, userId string) string {
+	urlHashBytes := sha256Of(intialLink + userId)
+	generatedNumber := new(big.Int).SetBytes(urlHashBytes).Uint64()
+	finalString := base58Encoded([]byte(fmt.Sprintf("%d", generatedNumber)))
+	return finalString[:8]
+}
