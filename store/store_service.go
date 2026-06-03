4e27358f574d90ad64e2363663b205a26f67cbb9
shx-dow <83426772+shx-dow@users.noreply.github.com>
2026-06-03 19:20:44 +0530

    Add Redis store initialization

--- store/store_service.go
+++ store/store_service.go
@@ -2,6 +2,7 @@ package store
 
 import (
 	"context"
+	"fmt"
 	"time"
 
 	"github.com/go-redis/redis"
@@ -17,3 +18,22 @@ var (
 )
 
 const CacheDuration = 6 * time.Hour
+
+func InitializeStore() *StorageService {
+	redisClient := redis.NewClient(&redis.Options{
+		Addr:     "localhost:6379",
+		Password: "",
+		DB:       0,
+	})
+
+	pong, err := redisClient.Ping(ctx).Result()
+
+	if err != nil {
+		panic(fmt.Sprintf("Error init Redis: %v", err))
+	}
+
+	fmt.Printf("\nRedis started successfully: pong message = {%s}", pong)
+
+	storeService.redisClient = redisClient
+	return storeService
+}
