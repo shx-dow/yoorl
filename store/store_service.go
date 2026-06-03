414f94fabe9a6e6d86d31fe5cf90fa44caacee90
shx-dow <83426772+shx-dow@users.noreply.github.com>
2026-06-03 19:29:11 +0530

    Use Redis for URL mappings

--- store/store_service.go
+++ store/store_service.go
@@ -5,7 +5,7 @@ import (
 	"fmt"
 	"time"
 
-	"github.com/go-redis/redis"
+	"github.com/go-redis/redis/v8"
 )
 
 type StorageService struct {
@@ -36,4 +36,19 @@ func InitializeStore() *StorageService {
 
 	storeService.redisClient = redisClient
 	return storeService
+}
+
+func SaveUrlMapping(shortUrl string, originalUrl string, userId string) {
+	err := storeService.redisClient.Set(ctx, shortUrl, originalUrl, CacheDuration).Err()
+	if err != nil {
+		panic(fmt.Sprintf("Failed saving key url | Error: %v - shortUrl: %s - originalUrl: %s\n", err, shortUrl, originalUrl))
+	}
+}
+
+func RetrieveInitialUrl(shortUrl string) string {
+	result, err := storeService.redisClient.Get(ctx, shortUrl).Result()
+	if err != nil {
+		panic(fmt.Sprintf("Failed RetrieveInitialUrl url | Error: %v - shortUrl: %s\n", err, shortUrl))
+	}
+	return result
 }
