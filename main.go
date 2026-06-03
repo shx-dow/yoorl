0aea94df0b89585254527c03081eedd7a07d15a2
shx-dow <83426772+shx-dow@users.noreply.github.com>
2026-06-03 23:15:13 +0530

    Add short URL creation and redirect routes

--- main.go
+++ main.go
@@ -4,6 +4,8 @@ import (
 	"fmt"
 
 	"github.com/gin-gonic/gin"
+	"github.com/shx-dow/yoorl/handler"
+	"github.com/shx-dow/yoorl/store"
 )
 
 func main() {
@@ -13,6 +15,16 @@ func main() {
 			"message": "URL Shortener",
 		})
 	})
+
+	r.POST("/create-short-url", func(c *gin.Context) {
+		handler.CreateShortUrl(c)
+	})
+
+	r.GET("/:shortUrl", func(c *gin.Context) {
+		handler.HandleShortUrlRedirect(c)
+	})
+
+	store.InitializeStore()
 
 	err := r.Run(":9808")
 	if err != nil {
