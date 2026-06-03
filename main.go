fea31c28a99b6e271be007a8ad2dd30b07262662
shx-dow <83426772+shx-dow@users.noreply.github.com>
2026-06-03 18:59:14 +0530

    Add Gin HTTP server

--- main.go
+++ main.go
@@ -1,7 +1,21 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+
+	"github.com/gin-gonic/gin"
+)
 
 func main() {
-	fmt.Printf("URL Shortener")
+	r := gin.Default()
+	r.GET("/", func(c *gin.Context) {
+		c.JSON(200, gin.H{
+			"message": "URL Shortener",
+		})
+	})
+
+	err := r.Run(":9808")
+	if err != nil {
+		panic(fmt.Sprintf("Failed to start the web server - Error: &v", err))
+	}
 }
